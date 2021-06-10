d349880a6d6c16840cdee71fbe9087e99f9a5125
Agustin Valdes <42925417+dasgrossfuchs@users.noreply.github.com>
2021-06-09 20:57:40 -0700

    actividad

--- routes/routes.js
+++ routes/routes.js
@@ -17,5 +17,32 @@ router.get('/about',(req,res)=>{
 });
 
 router.get('/clase',(req,res)=>{
-    res.render('pages/multimedia',{title:'video'});
+    var clase = {
+        tipo :'video',
+        matname:'estudio.mp4#t=10',
+        matbck:'http://placekitten.com/600/350',
+        mattitulo:'¿Que es una escuela?',
+        texttitulo:'Este es un titulo',
+        textcont:`Este es el contenido que contiene el 
+        texto contenido por el contenedor del texto que 
+        contiene texto para contener el texto descrito 
+        por la actividad de texto para texto, si por 
+        algún motivo contuviera algún otro tipo de 
+        contenido que no fuese posiblemente no serviría 
+        de nada.`,
+        instrucciones:['Lee el texto','Ve el video']
+    }
+    if(clase.tipo == 'video'){
+        clase.mattitulo = '&#127909; ' + clase.mattitulo;
+    }
+    console.log(clase);
+    res.render('pages/multimedia',{title:'video', clase});
+});
+
+router.get('/actividad',(req,res)=>{
+    res.render('pages/actividad',{title:'actividad'});
+});
+
+router.get('/clase2',(req,res)=>{
+    res.render('pages/audio',{title:'audio'});
 });

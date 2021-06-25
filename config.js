093a0873f37afcdf408d7af01c9e52d558f65328
Agustin Valdes <42925417+dasgrossfuchs@users.noreply.github.com>
2021-06-24 21:16:36 -0700

    ss

--- config.js
+++ config.js
@@ -1,14 +1,17 @@
+process.env.USER = 'proyectmanager';
+process.env.PWD = '4TrcYYTtXjFKuyOd';
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PWD}@appis.65uar.mongodb.net/FuryDB?retryWrites=true&w=majority`;
 module.exports={
     port: process.env.PORT || 8080,
-    db: process.env.MONGODB || 'mongodb://localhost:27017/FuryDB',
-    // db: process.env.MONGODB || "mongodb+srv://proyectmanager:4TrcYYTtXjFKuyOd@appis.65uar.mongodb.net/FuryDB",
+    db: process.env.MONGODB || uri,
     urlParser : { 
         useNewUrlParser: true,
         useUnifiedTopology: true, 
         useFindAndModify:false,
         useCreateIndex:true 
     } 
-}
+};
+// db: process.env.MONGODB || 'mongodb://localhost:27017/FuryDB',
 //proyectmanager
 //4TrcYYTtXjFKuyOd
 //const uri = "mongodb+srv://proyectmanager:4TrcYYTtXjFKuyOd@appis.65uar.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
